cb12cb0fc3d1b91abac8e2f90ebea95c78be518d
Bluewater <bluewater@Bluewaters-MacBook-Pro.local>
2022-10-12 00:26:02 -0500

    emojis looking nice

--- src/learning_cat_chat/Main.java
+++ src/learning_cat_chat/Main.java
@@ -9,6 +9,12 @@ public class Main {
     public static void main(String[] args) {
 
         new Cat("Meow...");
+        new Dog("Woof!...woof");
+        new Sheep("What do I say?");
+        new Chicken("Cluck Cluck");
+        new Cow("Moo!..");
+        new Goat("Bheee!...");
+        new Monkey("Hello, gentlemen.");
 
 
     }
@@ -101,6 +107,12 @@ public class Main {
             return this.animalImage;
         }
 
+        public void formatConsoleOutput(String image, String message){
+
+            System.out.println(image + ": " + message);
+
+        }
+
 
    /*  public String setAnimalImage{
             EnumSet.allOf(Type.class).forEach(type -> {
@@ -120,39 +132,74 @@ public class Main {
     }
 
     public static class Cat extends Animal {
-        private String catImg;
-        
+        private String catImg = "\uD83D\uDE3A";
 
         public Cat(String myAnimalSays) {
 
             super(myAnimalSays);
-            setAnimalType(Type.CAT);
-            if (getAnimalType() == Type.CAT) getCatImg();
+            formatConsoleOutput(catImg, myAnimalSays);
         }
 
-        public String getCatImg() {
-            return this.catImg;
+    }
+
+    public static class Dog extends Animal {
+
+        private String dogImg = "\uD83D\uDC15";
+
+        public Dog(String myAnimalSays) {
+            super(myAnimalSays);
+            formatConsoleOutput(dogImg, myAnimalSays);
+        }
+    }
+    public static class Sheep extends Animal {
+        private String sheepImg = "\uD83D\uDC11";
+
+        public Sheep(String myAnimalSays) {
+
+            super(myAnimalSays);
+            formatConsoleOutput(sheepImg, myAnimalSays);
         }
 
-        public void setCatImgHelper(String catImg) {
-            EnumSet.allOf(Type.class).forEach(type -> {
-                try {
-                    if (type == Type.CAT) setCatImg(animalEmojis.get(type));{
-                    }
-                } catch (Exception e){
-                    System.out.println(e + "Ain't no cat image");
-                }
+    }
+    public static class Chicken extends Animal {
+        private String chickenImg = "\uD83D\uDC14";
 
-            });
+        public Chicken(String myAnimalSays) {
+
+            super(myAnimalSays);
+            formatConsoleOutput(chickenImg, myAnimalSays);
         }
 
+    }
+    public static class Cow extends Animal {
+        private String cowImg = "\uD83D\uDC2E";
 
-        public String setCatImg(String catImg) {
-            return this.catImg = catImg;
+        public Cow(String myAnimalSays) {
+
+            super(myAnimalSays);
+            formatConsoleOutput(cowImg, myAnimalSays);
         }
 
+    }
+    public static class Goat extends Animal {
+        private String goatImg = "\uD83D\uDC10";
 
+        public Goat(String myAnimalSays) {
 
+            super(myAnimalSays);
+            formatConsoleOutput(goatImg, myAnimalSays);
+        }
+
+    }
+
+    public static class Monkey extends Animal {
+        private String monkeyImg = "\uD83D\uDC12";
+
+        public Monkey(String myAnimalSays) {
+
+            super(myAnimalSays);
+            formatConsoleOutput(monkeyImg, myAnimalSays);
+        }
 
     }
 }
