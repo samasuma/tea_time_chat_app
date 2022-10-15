3541c21724ebc34eb7a24cad8779bc890988b6af
Bluewater <bluewater@Bluewaters-MacBook-Pro.local>
2022-10-15 09:23:51 -0500

    added care features

--- src/learning_cat_chat/Main.java
+++ src/learning_cat_chat/Main.java
@@ -1,38 +1,78 @@
 package learning_cat_chat;
 
+
+import learning_cat_chat.care.ScheduleCare;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static void main(String[] args) {
 
         new Cat("Meow...");
-        new Dog("Woof!...woof");
-        new Sheep("What do I say?");
-        new Chicken("Cluck Cluck");
-        new Cow("Moo!..");
-        new Goat("Bheee!...");
-        new Monkey("Hello, gentlemen.");
+
+
+
 
 
     }
 
-    public static class Animal {
-        private String animalSays;
+    public static class Pet implements ScheduleCare {
+        private String petSays;
+        private String eatLanguage;
+
+        @Override
+        public String feedReminder() {
+            return null;
+        }
+
+        @Override
+        public String groomingReminder() {
+            return null;
+        }
+
+        @Override
+        public String vetReminder() {
+            return null;
+        }
+
+        @Override
+        public String tidyPetEnv() {
+            return null;
+        }
 
         public enum Type {
             CAT, DOG, SHEEP, CHICKEN,
             COW, GOAT, MONKEY
         }
 
-        public Animal(String myAnimalSays) {
-            setAnimalSound(myAnimalSays);
+        public Pet(String myPetSays) {
+            setPetSound(myPetSays);
+            eatTime();
+
         }
 
         /**
-         * @param myAnimalSound :a string value that sets your cat's response.
+         * @param myPetSound :a string value that sets your cat's response.
          */
-        public void setAnimalSound(String myAnimalSound) {
-            this.animalSays = myAnimalSound;
+        public void setPetSound(String myPetSound) {
+            this.petSays = myPetSound;
         }
+
+        public void eatTime(){
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println("Running: " + new java.util.Date());
+                }
+            }, 0, 5000);
+        }
+
 
         public void formatConsoleOutput(String image, String message){
             System.out.println(image + ": " + message);
@@ -40,75 +80,91 @@ public class Main {
 
     }
 
-    public static class Cat extends Animal {
+    public static class Cat extends Pet {
         private String myCatImg = "\uD83D\uDE3A";
-        private Type myAnimalType = Type.CAT;
+        private Type myPetType = Type.CAT;
+        private String catHungry = "I'm hungry. Grr...";
 
-        public Cat(String myAnimalSays) {
+        public Cat(String myPetSays) {
 
-            super(myAnimalSays);
-            formatConsoleOutput(myCatImg, myAnimalSays);
+            super(myPetSays);
+            formatConsoleOutput(myCatImg, myPetSays);
+
+
         }
+        //Schedule eating time idea. Bug:null catHungry when first running. Come up with better scheduler.
+        @Override
+        public void eatTime() {
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
 
+                    formatConsoleOutput(myCatImg, catHungry);
+
+                }
+            }, 0, 5000);
+
+        }
     }
 
-    public static class Dog extends Animal {
+    public static class Dog extends Pet {
 
         private String myDogImg = "\uD83D\uDC15";
 
-        public Dog(String myAnimalSays) {
-            super(myAnimalSays);
-            formatConsoleOutput(myDogImg, myAnimalSays);
+        public Dog(String myPetSays) {
+            super(myPetSays);
+            formatConsoleOutput(myDogImg, myPetSays);
         }
     }
-    public static class Sheep extends Animal {
+    public static class Sheep extends Pet {
         private String mySheepImg = "\uD83D\uDC11";
 
-        public Sheep(String myAnimalSays) {
+        public Sheep(String myPetSays) {
 
-            super(myAnimalSays);
-            formatConsoleOutput(mySheepImg, myAnimalSays);
+            super(myPetSays);
+            formatConsoleOutput(mySheepImg, myPetSays);
         }
 
     }
-    public static class Chicken extends Animal {
+    public static class Chicken extends Pet {
         private String myChickenImg = "\uD83D\uDC14";
 
-        public Chicken(String myAnimalSays) {
+        public Chicken(String myPetSays) {
 
-            super(myAnimalSays);
-            formatConsoleOutput(myChickenImg, myAnimalSays);
+            super(myPetSays);
+            formatConsoleOutput(myChickenImg, myPetSays);
         }
 
     }
-    public static class Cow extends Animal {
+    public static class Cow extends Pet {
         private String myCowImg = "\uD83D\uDC2E";
 
-        public Cow(String myAnimalSays) {
+        public Cow(String myPetSays) {
 
-            super(myAnimalSays);
-            formatConsoleOutput(myCowImg, myAnimalSays);
+            super(myPetSays);
+            formatConsoleOutput(myCowImg, myPetSays);
         }
 
     }
-    public static class Goat extends Animal {
+    public static class Goat extends Pet {
         private String myGoatImg = "\uD83D\uDC10";
 
-        public Goat(String myAnimalSays) {
+        public Goat(String myPetSays) {
 
-            super(myAnimalSays);
-            formatConsoleOutput(myGoatImg, myAnimalSays);
+            super(myPetSays);
+            formatConsoleOutput(myGoatImg, myPetSays);
         }
 
     }
 
-    public static class Monkey extends Animal {
+    public static class Monkey extends Pet {
         private String myMonkeyImg = "\uD83D\uDC12";
 
-        public Monkey(String myAnimalSays) {
+        public Monkey(String myPetSays) {
 
-            super(myAnimalSays);
-            formatConsoleOutput(myMonkeyImg, myAnimalSays);
+            super(myPetSays);
+            formatConsoleOutput(myMonkeyImg, myPetSays);
         }
 
     }
---
+++ src/learning_cat_chat/care/ScheduleCare.java
@@ -0,0 +1,9 @@
+package learning_cat_chat.care;
+
+public interface ScheduleCare {
+
+    public String feedReminder();
+    public String groomingReminder();
+    public String vetReminder();
+    public String tidyPetEnv();
+}
